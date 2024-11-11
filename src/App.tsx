59b1a68c5e25283561ac7880aa2aab9f8efbb756
CandyDog13 <kga13@yandex.ru>
2024-11-11 12:14:51 +0300

    add media

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Pays from "./pages/Pays/pays";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/sertificate-app" element={<HomePage />} />
       <Route path="/pays" element={<Pays/>} />
     </Routes>
   );
